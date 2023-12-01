2703b4021015ce9960536f2f88c35a6c90866b99
Dion Trilsbeek <d.trilsbeek@betabit.nl>
2023-12-01 13:18:53 +0100

    finish day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.RegularExpressions;
+using Day1;
+
+Console.WriteLine("Hello, AdventOfCode2023!");
+
+var testDirections = new List<string>
+{
+    "(())",
+    "()()",
+    "(((",
+    "(()(()(",
+    "))(((((",
+    "())",
+    "))(",
+    ")))",
+    ")())())"
+};
+// testDirections.ForEach(d => Console.WriteLine(SantaFloorFinder.Find(d)));
+// testDirections.ForEach(d => Console.WriteLine(SantaFloorFinder.Find_V2(d)));
+
+// var actualDirections = await File.ReadAllTextAsync("./inputPuzzle_1.txt");
+
+// Console.WriteLine(SantaFloorFinder.Find(actualDirections));
+// Console.WriteLine(SantaFloorFinder.Find_V2(actualDirections));
+
+var calibrationLines = await File.ReadAllLinesAsync("./inputPuzzle_2.txt");
+// var calibrationLines = await File.ReadAllLinesAsync("./TestInput.txt");
+
+var numbersText = new Dictionary<string, string>
+{
+   {"one", "o1e"},
+   {"two", "t2o"},
+   {"three", "t3e"},
+   {"four", "f4r"},
+   {"five", "f5e"},
+   {"six","s6x"},
+   {"seven", "s7n"},
+   {"eight", "e8t"},
+   {"nine", "n9e"},
+};
+
+var calibration = new List<int>();
+foreach (var line in calibrationLines)
+{
+    var pattern = @"(one|two|three|four|five|six|seven|eight|nine)";
+    
+    bool didMatch;
+    var convertedLine = line;
+
+    do
+    {
+        didMatch = false;
+        convertedLine = Regex.Replace(convertedLine, pattern, match =>
+        {
+            didMatch = true;
+            var matchValue = match.Value.ToLowerInvariant();
+            return numbersText[matchValue];
+        });
+    } while (didMatch);
+    
+    var digits = convertedLine.Where(char.IsDigit).ToList();
+    var stringNumber = string.Join("", digits.First(), digits.Last());
+    var finalNumber = int.Parse(stringNumber);
+    Console.WriteLine(finalNumber + " " + line);
+    
+    calibration.Add(finalNumber);
+}
+
+var result = calibration.Sum();
+
+Console.WriteLine(result);
+
+
---
+++ Day1/SantaFloorFinder.cs
@@ -0,0 +1,38 @@
+﻿namespace Day1;
+
+public class SantaFloorFinder
+{
+    public static int Find(string directions)
+    {
+        var storiesUp = directions.Count(f => f.Equals('('));
+        var storiesDown = directions.Count(f => f.Equals(')'));
+        
+        return storiesUp - storiesDown;
+    }
+    
+    public static int Find_V2(string directions)
+    {
+        var floor = 0;
+        var position = 0;
+        foreach (var direction in directions)
+        {
+            position++;
+            switch (direction)
+            {
+                case '(':
+                    floor++;
+                    break;
+                case ')':
+                    floor--;
+                    break;
+            }
+
+            if (floor == -1)
+            {
+                Console.WriteLine($"Santa entered the basement at position {position}");
+            }
+        }
+
+        return floor;
+    }
+}
