2c3153ee628b6169afd82855f5c152df59e132a3
Dion <d.trilsbeek@gmail.com>
2023-12-02 13:09:26 +0100

    setup

---
+++ Day2/Program.cs
@@ -0,0 +1,12 @@
+﻿using Day2;
+
+var gameLines = await File.ReadAllLinesAsync("./input.txt");
+
+var i = 1;
+foreach (var line in gameLines)
+{
+	var bag = new Bag(new Cubes(1,5,2));
+	var game = new Game(i, line, bag);
+	
+	i++;
+}
