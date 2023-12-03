1f22fa815719d962d688445f40083d9b17d78dec
Dion <d.trilsbeek@gmail.com>
2023-12-03 16:40:33 +0100

    day 3 part 2

--- Day3/Line.cs
+++ Day3/Line.cs
@@ -6,56 +6,90 @@ public class Line
 {
 	public int Key { get; }
 	public string Data { get; }
+	public Dictionary<string, Gear> Gears { get; }
 
-	public Line(int key, string data)
+	public Line(int key, string data, Dictionary<string, Gear> gears)
 	{
 		Key = key;
 		Data = data;
+		Gears = gears;
 	}
 
-	public List<int> GetEnginePartNumbers(Dictionary<int, Line> lines)
+	public List<Part> GetEnginePartNumbers(Dictionary<int, Line> lines)
 	{
 		var locations = Regex.Matches(Data, @"\d+");
-		var partNumbers = new List<int>();
+		var parts = new List<Part>();
 		foreach (Match location in locations)
 		{
-			// Console.WriteLine($"location: {location.Index} : {location.Value}");
-
-			if (location.Value.Where((t, i) => CheckAdjecent(location.Index + i, lines)).Any())
+			var part = new Part(int.Parse(location.Value));
+			
+			if (location.Value.Where((t, i) => CheckAdjecent(location.Index + i, part, lines)).Any())
 			{
-				partNumbers.Add(int.Parse(location.Value));
+				parts.Add(part);
 			}
 		}
 
-		return partNumbers;
+		return parts;
 	}
 
-	private bool CheckAdjecent(int location, Dictionary<int, Line> lines)
+	private bool CheckAdjecent(int location, Part part, Dictionary<int, Line> lines)
 	{
 		lines.TryGetValue(Key - 1, out var above);
 		lines.TryGetValue(Key + 1, out var below);
 
-		var adjecent = new List<char?>();
-		adjecent.Add(above?.Data.ElementAtOrDefault( location-1 ));
-		adjecent.Add(above?.Data.ElementAtOrDefault(location));
-		adjecent.Add( above?.Data.ElementAtOrDefault( location+1));
-		adjecent.Add( Data.ElementAtOrDefault( location-1));
-		adjecent.Add( Data.ElementAtOrDefault( location+1));
-		adjecent.Add( below?.Data.ElementAtOrDefault(location -1));
-		adjecent.Add(below?.Data.ElementAtOrDefault(location));
-		adjecent.Add( below?.Data.ElementAtOrDefault(location +1));
+		var adjecent = new List<AdjecentChar>();
+		adjecent.Add(new AdjecentChar(above?.Data, (Key -1, location-1)));
+		adjecent.Add(new AdjecentChar(above?.Data, (Key -1, location) ));
+		adjecent.Add(new AdjecentChar( above?.Data, (Key -1, location+1) ));
+		adjecent.Add(new AdjecentChar( Data, (Key, location-1) ));
+		adjecent.Add(new AdjecentChar( Data, (Key, location+1) ));
+		adjecent.Add(new AdjecentChar( below?.Data, (Key+1, location-1) ));
+		adjecent.Add(new AdjecentChar(below?.Data, (Key+1, location) ));
+		adjecent.Add(new AdjecentChar( below?.Data, (Key+1, location+1) ));
 
 		return adjecent.Any(a =>
 		{
-			if (a == null) return false;
-			if (a.Value == '.') return false;
-			if (a.Value == '\0') return false;
-			if (char.IsDigit(a.Value)) return false;
+			if (a.Char == null) return false;
+			if (a.Char.Value == '.') return false;
+			if (a.Char.Value == '\0') return false;
+			if (char.IsDigit(a.Char.Value)) return false;
+			if (a.Char.Value == '*')
+			{
+				var gearKey = $"{a.Position.y}_{a.Position.x}";
+				Gears.TryGetValue(gearKey, out var existing);
 
-
-			Console.WriteLine(a.Value);
+				if (existing != null)
+				{
+					if (!existing.Parts.Exists(p => p == part))
+					{
+						existing.Parts.Add(part);
+					}
+				}
+				else
+				{
+					Gears[gearKey] = new Gear(part);
+				}
+			}
 
 			return true;
 		});
 	}
+}
+
+public class Gear(Part part)
+{
+	public List<Part> Parts = new()
+	{
+		part
+	};
+}
+public class AdjecentChar(string? value, (int y, int x) position)
+{
+	public char? Char { get; } = value?.ElementAtOrDefault(position.x);
+	public (int y, int x) Position { get; } = position;
+}
+
+public class Part(int number)
+{
+	public int Number { get; } = number;
 }
--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,14 +1,21 @@
 ﻿using Day3;
-
+var gears = new Dictionary<string, Gear>();
 var lines = (await File.ReadAllLinesAsync("./input.txt"))
-	.Select((line, key) => new KeyValuePair<int,Line>(key, new Line(key, line)) )
+	.Select((line, key) => new KeyValuePair<int,Line>(key, new Line(key, line, gears)) )
 	.ToDictionary();
 
-var allPartNumbers = new List<int>();
+var allPartNumbers = new List<Part>();
+
 foreach (var (_, line) in lines)
 {
 	allPartNumbers.AddRange(line.GetEnginePartNumbers(lines));
 }
 
-var result =allPartNumbers.Sum();
+var result =allPartNumbers.Sum( p => p.Number);
 Console.WriteLine(result);
+
+var result2 =gears.Where(g => g.Value.Parts.Count == 2)
+	.Sum(g => g.Value.Parts[0].Number * g.Value.Parts[1].Number);
+
+Console.WriteLine(result2);
+
