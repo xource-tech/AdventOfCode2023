0e9c2c2383026900703d1a2bda7e1ecc8505ec5c
Dion <d.trilsbeek@gmail.com>
2023-12-03 15:35:12 +0100

    day 3 part 1

---
+++ Day3/Line.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day3;
+
+public class Line
+{
+	public int Key { get; }
+	public string Data { get; }
+
+	public Line(int key, string data)
+	{
+		Key = key;
+		Data = data;
+	}
+
+	public List<int> GetEnginePartNumbers(Dictionary<int, Line> lines)
+	{
+		var locations = Regex.Matches(Data, @"\d+");
+		var partNumbers = new List<int>();
+		foreach (Match location in locations)
+		{
+			// Console.WriteLine($"location: {location.Index} : {location.Value}");
+
+			if (location.Value.Where((t, i) => CheckAdjecent(location.Index + i, lines)).Any())
+			{
+				partNumbers.Add(int.Parse(location.Value));
+			}
+		}
+
+		return partNumbers;
+	}
+
+	private bool CheckAdjecent(int location, Dictionary<int, Line> lines)
+	{
+		lines.TryGetValue(Key - 1, out var above);
+		lines.TryGetValue(Key + 1, out var below);
+
+		var adjecent = new List<char?>();
+		adjecent.Add(above?.Data.ElementAtOrDefault( location-1 ));
+		adjecent.Add(above?.Data.ElementAtOrDefault(location));
+		adjecent.Add( above?.Data.ElementAtOrDefault( location+1));
+		adjecent.Add( Data.ElementAtOrDefault( location-1));
+		adjecent.Add( Data.ElementAtOrDefault( location+1));
+		adjecent.Add( below?.Data.ElementAtOrDefault(location -1));
+		adjecent.Add(below?.Data.ElementAtOrDefault(location));
+		adjecent.Add( below?.Data.ElementAtOrDefault(location +1));
+
+		return adjecent.Any(a =>
+		{
+			if (a == null) return false;
+			if (a.Value == '.') return false;
+			if (a.Value == '\0') return false;
+			if (char.IsDigit(a.Value)) return false;
+
+
+			Console.WriteLine(a.Value);
+
+			return true;
+		});
+	}
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,14 @@
+﻿using Day3;
+
+var lines = (await File.ReadAllLinesAsync("./input.txt"))
+	.Select((line, key) => new KeyValuePair<int,Line>(key, new Line(key, line)) )
+	.ToDictionary();
+
+var allPartNumbers = new List<int>();
+foreach (var (_, line) in lines)
+{
+	allPartNumbers.AddRange(line.GetEnginePartNumbers(lines));
+}
+
+var result =allPartNumbers.Sum();
+Console.WriteLine(result);
