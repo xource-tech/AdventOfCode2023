039ef5d01367268430228d4b340f0e51ec4698ee
Dion <d.trilsbeek@gmail.com>
2023-12-06 12:35:58 +0100

    add day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day6;
+
+Console.WriteLine("Hello, World!");
+
+var testRaces = new List<Race>
+{
+	new(7, 9),
+	new(15, 40),
+	new(30, 200)
+};
+
+var races = new List<Race>
+{
+	new(45, 295),
+	new(98, 1734),
+	new(83, 1278),
+	new(73, 1210)
+};
+
+var partTwoTestRace = new Race(71530, 940200);
+var partTwoRace = new Race(45988373, 295173412781210);
+
+var amountWinningContenders = races.Select(race => race.Contenders.Count(c => c.Distance > race.RecordDistance)).ToList();
+
+var total = amountWinningContenders.Aggregate((x,y) => x * y);
+
+Console.WriteLine(total);
+
+var result2 = partTwoRace.Contenders.Count(c => c.Distance > partTwoRace.RecordDistance);
+
+Console.WriteLine(result2);
---
+++ Day6/Race.cs
@@ -0,0 +1,24 @@
+﻿namespace Day6;
+
+public class Race
+{
+	public int TimeLimit { get; }
+	public long RecordDistance { get; }
+	public List<Contender> Contenders { get; } = new();
+
+	public Race(int timeLimit, long recordDistance)
+	{
+		TimeLimit = timeLimit;
+		RecordDistance = recordDistance;
+		for (var i = 1; i < timeLimit; i++)
+		{
+			Contenders.Add(new Contender(i, timeLimit));
+		}
+	}
+}
+
+public class Contender(long chargingTime, long timeLimit)
+{
+	public long ChargingTime { get; } = chargingTime;
+	public long Distance { get; } = chargingTime * (timeLimit - chargingTime);
+}
