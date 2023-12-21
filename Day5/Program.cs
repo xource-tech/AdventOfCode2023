6caf375eb61a6f10e01b18834066e5616ed45f7d
Dion Trilsbeek <d.trilsbeek@betabit.nl>
2023-12-21 16:55:29 +0100

    partial day 5

---
+++ Day5/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day5;
+
+Console.WriteLine("Hello, World!");
+
+// Read Maps to classes
+var lines = await File.ReadAllLinesAsync("input.txt");
+var seeds = lines[0].Split(':')[1].Split(' ').Select(long.Parse).ToList();
+var maps = new Dictionary<string, IEnumerable<FoodProductionMap>>();
+
+string currentMapName;
+var currentMapLines = new List<string>();
+
+for (var index = 1; index < lines.Length; index++)
+{
+    var line = lines[index];
+    if (line.Contains("-to-"))
+    {
+        currentMapName = line.Trim();
+        maps[currentMapName] = currentMapLines
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .Select(x => x.Split(' ').Select(long.Parse).ToArray())
+            .Select(v => new FoodProductionMap(v[0], v[1], v[2]));
+
+        currentMapLines.Clear();
+    }
+    else
+    {
+        currentMapLines.Add(line);
+    }
+}
+
+var finder = new LowestLocationFinder(seeds, maps);
+var result1 = finder.Puzzle1();
+
+Console.WriteLine("hi");
---
+++ Day5/SeedToSoil.cs
@@ -0,0 +1,39 @@
+﻿namespace Day5;
+
+public class FoodProductionMap(long sourceRangeStart, long destRangeStart, long rangeLength)
+{
+    public long SourceRangeStart { get; } = sourceRangeStart;
+    public long DestRangeStart { get; } = destRangeStart;
+    public long RangeLength { get; } = rangeLength;
+
+    public long Lowest(long seed)
+    {
+        // diff source / dest
+        return seed;
+    }
+}
+
+public class LowestLocationFinder(List<long> seeds, Dictionary<string, IEnumerable<FoodProductionMap>> maps)
+{
+    public List<long> Seeds { get; } = seeds;
+    public Dictionary<string, IEnumerable<FoodProductionMap>> Maps { get; } = maps;
+
+    public long Puzzle1()
+    {
+        // Keep track of lowest location when looking up by key (calculate, not produce all ranges)
+        var lowest = 0;
+
+        foreach (var seed in Seeds)
+        {
+            var mapLowest = 0;
+            
+            foreach (var map in maps)
+            {
+                
+            }
+        }
+        // For all of the seeds, which is the lowest location?
+
+        return lowest;
+    }
+}
